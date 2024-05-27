51a37ce6187b0ff8a669f7b89ab0292e94be77da
Rohan Mars <code@rohanmars.com>
2024-05-27 14:05:21 -0700

    support testing in repos with uppercase chars

--- src/ci-tester.ts
+++ src/ci-tester.ts
@@ -135,7 +135,7 @@ export async function run(): Promise<void> {
   }
   if (args.repository) {
     assertString(args.repository)
-    config.repository = args.repository
+    config.repository = args.repository.toLowerCase()
   }
   if (args.package) {
     assertString(args.package)
